4b73acd1d8913d85dab85329d2ab782be472579c
microwavetaco67 <arthurmascher9@gmail.com>
2025-12-28 14:31:36 +0100

    Datagen and new Block

--- src/main/java/micro/drugs/DrugModDataGenerator.java
+++ src/main/java/micro/drugs/DrugModDataGenerator.java
@@ -1,6 +1,8 @@
 package micro.drugs;
 
 import micro.drugs.datagen.ModBlockTagProvider;
+import micro.drugs.datagen.ModItemTagProvider;
+import micro.drugs.datagen.ModLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -9,7 +11,9 @@ public class DrugModDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
 
 	}
 }
--- src/main/java/micro/drugs/datagen/ModItemTagProvider.java
+++ src/main/java/micro/drugs/datagen/ModItemTagProvider.java
@@ -1,4 +1,20 @@
 package micro.drugs.datagen;
 
-public class ModItemTagProvider {
+import micro.drugs.util.ModTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS);
+
+    }
 }
